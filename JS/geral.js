6af15a08d867794da6436001c20bb661b28a8f7d
pedrocovisi <covisip@gmail.com>
2025-03-31 08:40:07 -0300

    quase pronto

--- JS/geral.js
+++ JS/geral.js
@@ -2,15 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnClaro = document.getElementById('btnclaro');
     const btnEscuro = document.getElementById('btnescuro');
 
-    // Função para alternar para o modo claro
+    // Verifica o tema salvo no localStorage e aplica na página
+    const modoSalvo = localStorage.getItem("modo-tema");
+
+    if (modoSalvo === "escuro") {
+        document.body.classList.add('escuro');
+        document.body.classList.remove('claro');
+        btnEscuro.checked = true;
+    } else {
+        document.body.classList.add('claro');
+        document.body.classList.remove('escuro');
+        btnClaro.checked = true;
+    }
+
+    // Função para alternar para o modo claro e salvar no localStorage
     btnClaro.addEventListener('click', () => {
         document.body.classList.add('claro');
         document.body.classList.remove('escuro');
+        localStorage.setItem("modo-tema", "claro");
     });
 
-    // Função para alternar para o modo escuro
+    // Função para alternar para o modo escuro e salvar no localStorage
     btnEscuro.addEventListener('click', () => {
         document.body.classList.add('escuro');
         document.body.classList.remove('claro');
+        localStorage.setItem("modo-tema", "escuro");
     });
 });
