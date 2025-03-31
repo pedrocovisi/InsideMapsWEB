7467ea6e87d05e35cd11fa1c0f9f0dcfad71ba1a
pedrocovisi <covisip@gmail.com>
2025-03-31 09:47:18 -0300

    tentando concertar o bug

--- JS/geral.js
+++ JS/geral.js
@@ -1,31 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnClaro = document.getElementById('btnclaro');
     const btnEscuro = document.getElementById('btnescuro');
+    const body = document.body;
 
-    // Verifica o tema salvo no localStorage e aplica na página
-    const modoSalvo = localStorage.getItem("modo-tema");
-
-    if (modoSalvo === "escuro") {
-        document.body.classList.add('escuro');
-        document.body.classList.remove('claro');
+    // Aplica o tema salvo no localStorage
+    const theme = localStorage.getItem("theme");
+    if (theme === "escuro") {
+        body.classList.add("escuro");
         btnEscuro.checked = true;
     } else {
-        document.body.classList.add('claro');
-        document.body.classList.remove('escuro');
+        body.classList.add("claro");
         btnClaro.checked = true;
     }
 
-    // Função para alternar para o modo claro e salvar no localStorage
-    btnClaro.addEventListener('click', () => {
-        document.body.classList.add('claro');
-        document.body.classList.remove('escuro');
-        localStorage.setItem("modo-tema", "claro");
+    // Função para alternar para o modo claro
+    btnClaro.addEventListener("click", () => {
+        body.classList.add("claro");
+        body.classList.remove("escuro");
+        localStorage.setItem("theme", "claro");
     });
 
-    // Função para alternar para o modo escuro e salvar no localStorage
-    btnEscuro.addEventListener('click', () => {
-        document.body.classList.add('escuro');
-        document.body.classList.remove('claro');
-        localStorage.setItem("modo-tema", "escuro");
+    // Função para alternar para o modo escuro
+    btnEscuro.addEventListener("click", () => {
+        body.classList.add("escuro");
+        body.classList.remove("claro");
+        localStorage.setItem("theme", "escuro");
     });
 });
